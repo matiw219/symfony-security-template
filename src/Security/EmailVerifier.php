15ed65f7126cd0c06941b94f9b4f737928f80a4b
matiw219 <mateusz.wielechowski1@gmail.com>
2023-08-24 18:04:12 +0200

    Add method which create EmailVerification code for user

--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -2,6 +2,9 @@
 
 namespace App\Security;
 
+use App\Entity\EmailVerification;
+use App\Entity\User;
+use App\Repository\EmailVerificationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -12,7 +15,21 @@ class EmailVerifier
         private MailerInterface $mailer,
         private EntityManagerInterface $entityManager,
         private UrlGeneratorInterface $urlGenerator,
+        private EmailVerificationRepository $emailVerificationRepository,
     ) {
+    }
+
+    public function createEmailVerification(User $user) : EmailVerification {
+        $code = hash('sha256', $user->getId() . ';' . $user->getEmail() . ';' . $user->getPassword() . ';' . time());
+        $email = (new EmailVerification())
+            ->setUser($user)
+            ->setCode($code)
+            ->setSendAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($email);
+        $this->entityManager->flush();
+
+        return $email;
     }
 
 }
