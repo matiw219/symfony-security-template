40cf7fa2e395f77dca04f3ae4bda27b227bd83ee
matiw219 <mateusz.wielechowski1@gmail.com>
2023-08-24 18:29:53 +0200

    Add redirect after authentication

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -54,10 +54,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_index'));
     }
 
     protected function getLoginUrl(Request $request): string
